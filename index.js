87ad611104aee828d7ed7424e380bb6871e89dc0
Premchand <premchandmali001@gmail.com>
2023-12-27 23:12:59 +0530

    testing of working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.send('working')
+})
 app.listen('3000',()=>{
     console.log('Backend is listing at port 3000')
 })
