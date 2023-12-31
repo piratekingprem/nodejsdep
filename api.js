846229e9545fc054bf8583b8bb2dc818bcf2d3a7
Premchand <premchandmali001@gmail.com>
2023-12-31 14:18:48 +0530

    changed the main script

--- api.js
+++ api.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const serverless = require('serverless-http')
 const app = express();
-const router = express.Router();
-
-
 
 app.get('/',(req,res)=>{
     res.send('working')
@@ -11,7 +7,3 @@ app.get('/',(req,res)=>{
 app.listen('3000',()=>{
     console.log('Backend is listing at port 3000')
 })
-
-
-app.use('/.netlify/functions/api',router);
-module.exports.handler = serverless(app);
